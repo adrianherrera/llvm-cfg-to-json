bc4e90e3890e3172edfe249b2d0c3e0e1e15c910
Adrian Herrera <adrian.herrera02@gmail.com>
2019-10-13 09:22:11 +1100

    cleanup python script
    Now handles multiple mains in a codebase

--- cfg_stats.py
+++ cfg_stats.py
@@ -9,6 +9,7 @@
 from __future__ import print_function
 
 import argparse
+from collections import defaultdict
 import glob
 import json
 import os
@@ -31,6 +32,10 @@ def parse_args():
     return parser.parse_args()
 
 
+def create_node(mod, func, identifier):
+    return '%s.%s.%s' % (mod, func, identifier)
+
+
 def main():
     args = parse_args()
 
@@ -40,15 +45,23 @@ def main():
 
     # Load all of the control flow graphs (CFG)
 
-    cfg_dict = {}
+    cfg_dict = defaultdict(dict)
+    entry_pts = []
     num_indirect_calls = 0
 
     for json_path in glob.glob(os.path.join(json_dir, 'cfg.*.json')):
         with open(json_path, 'r') as json_file:
             data = json.load(json_file)
+
+            # Build the CFG
+            module = data.pop('module')
             func = data.pop('function')
-            cfg_dict[func] = data
-            num_indirect_calls += data['indirect_calls']
+            cfg_dict[module][func] = data
+
+            # Collect other interesting stats
+            if func == args.entry:
+                entry_pts.append((module, func))
+            num_indirect_calls += data.pop('indirect_calls')
 
     # Turn the CFGs into a networkx graph
 
@@ -58,73 +71,75 @@ def main():
     cfg = nx.DiGraph()
 
     # Add intraprocedural edges
-    for func, func_dict in cfg_dict.items():
-        if func in blacklist:
-            continue
+    for mod, mod_dict in cfg_dict.items():
+        for func, func_dict in mod_dict.items():
+            if func in blacklist:
+                continue
 
-        # JSON edges may be `none`
-        if not func_dict.get('edges'):
-            continue
+            # JSON edges may be `none`
+            if not func_dict.get('edges'):
+                continue
 
-        for edge in func_dict['edges']:
-            cfg.add_edge('%s.%s' % (func, edge['src']),
-                         '%s.%s' % (func, edge['dst']))
+            for edge in func_dict['edges']:
+                cfg.add_edge(create_node(mod, func, edge['src']),
+                             create_node(mod, func, edge['dst']))
 
     # Add interprocedural edges
-    for func, func_dict in cfg_dict.items():
-        # JSON calls may be `none`
-        if not func_dict.get('calls'):
-            continue
-
-        for call in func_dict['calls']:
-            # Add forward edge
-            #
-            # If we don't know anything about the callee function (such as
-            # its entry block), skip it
-            callee = call['dst']
-            callee_dict = cfg_dict.get(callee, {})
-            if 'entry' not in callee_dict:
+    for mod, mod_dict in cfg_dict.items():
+        for func, func_dict in mod_dict.items():
+            # JSON calls may be `none`
+            if not func_dict.get('calls'):
                 continue
 
-            cfg.add_edge('%s.%s' % (func, call['src']),
-                         '%s.%s' % (callee, callee_dict['entry']))
+            for call in func_dict['calls']:
+                # Add forward edge
+                #
+                # If we don't know anything about the callee function (such as
+                # its entry block), skip it
+                callee = call['dst']
+                callee_dict = cfg_dict.get(callee, {})
+                if 'entry' not in callee_dict:
+                    continue
 
-            # Add backward edges (if they exist)
-            #
-            # JSON returns may be `none`
-            if not callee_dict.get('returns'):
-                continue
+                cfg.add_edge(create_node(mod, func, call['src']),
+                             create_node(mod, func, callee_dict['entry']))
 
-            for ret in callee_dict['returns']:
-                cfg.add_edge('%s.%s' % (callee, ret),
-                             '%s.%s' % (func, call['src']))
+                # Add backward edges (if they exist)
+                #
+                # JSON returns may be `none`
+                if not callee_dict.get('returns'):
+                    continue
+
+                for ret in callee_dict['returns']:
+                    cfg.add_edge(create_node(mod, callee, ret),
+                                 create_node(mod, func, call['src']))
 
     # Output to DOT
     if args.dot:
         print('Writing to cfg.dot...')
         write_dot(cfg, 'cfg.dot')
+        print()
 
     # The resulting CFG will probably not be strongly connected. So split the
     # CFG into weakly-connected components and find the one with our given entry
-    # point
-    sub_cfg = None
-    entry_node = '%s.%s' % (args.entry, cfg_dict[args.entry]['entry'])
-    for wcc in nx.weakly_connected_component_subgraphs(cfg):
-        if entry_node in wcc:
-            sub_cfg = wcc
-            break
+    # point(s)
+    for sub_cfg in nx.weakly_connected_component_subgraphs(cfg):
+        for entry_mod, entry_func in entry_pts:
+            entry_node = create_node(entry_mod, entry_func,
+                                     cfg_dict[entry_mod][entry_func]['entry'])
 
-    if not sub_cfg:
-        raise Exception('Could not find CFG with entry point `%s`' % args.entry)
+            if entry_node not in sub_cfg:
+                continue
 
-    num_bbs = sub_cfg.number_of_nodes()
-    num_edges = sub_cfg.size()
-    entry_eccentricity = nx.eccentricity(sub_cfg, v=entry_node)
+            num_bbs = sub_cfg.number_of_nodes()
+            num_edges = sub_cfg.size()
+            eccentricity = nx.eccentricity(sub_cfg, v=entry_node)
 
-    print('# basic blocks: %d' % num_bbs)
-    print('# edges: %d' % num_edges)
-    print('# indirect calls: %d' % num_indirect_calls)
-    print('eccentricity from `%s`: %d' % (entry_node, entry_eccentricity))
+            print('`%s.%s` stats' % (entry_mod, entry_func))
+            print('  num. basic blocks: %d' % num_bbs)
+            print('  num. edges: %d' % num_edges)
+            print('  eccentricity from `%s`: %d' % (entry_node, eccentricity))
+            print()
 
 
 if __name__ == '__main__':
